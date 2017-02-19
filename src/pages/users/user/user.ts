fc475b27b38d5e614ec85e82de10a32f1e3976ca
Frank Shi <contact@franks.hk>
2017-02-19 22:15:19 +0800

    Add navigation to last page and ROOT page function.

--- src/pages/users/user/user.ts
+++ src/pages/users/user/user.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NavParams } from 'ionic-angular';
+import { NavParams, NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-user',
@@ -8,9 +8,22 @@ import { NavParams } from 'ionic-angular';
 export class UserPage implements OnInit{
 	name: string;
 
-	constructor (private navParams: NavParams) {}
+	constructor (
+		private navParams: NavParams, 
+		private navCtrl: NavController
+	) {}
 
 	ngOnInit () {
 		this.name = this.navParams.get('userName');
+	}
+
+	goBack(){
+		// go back, remove the page
+		this.navCtrl.pop();
+	}
+
+	goBackToRoot(){
+		// go back, remove the page
+		this.navCtrl.popToRoot();
 	}
 }
